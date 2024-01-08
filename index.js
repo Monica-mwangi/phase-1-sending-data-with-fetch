7605e30ef14cdbb0e1817cf8b9c93f990717249d
Monica <monica@DESKTOP-A3SK9UM.localdomain>
2024-01-08 05:35:49 -0800

    done with assignements

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      const id = data.id;
+      document.body.innerHTML += `<p>${id}</p>`;
+    })
+    .catch(function(error) {
+      document.body.innerHTML += `<p>${error.message}</p>`;
+    })
+}
